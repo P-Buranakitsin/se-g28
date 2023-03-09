29f56bc382e06a1244d7e65b476f08db89b60b87
Wasi Wattanasiritham <2812576w@student.gla.ac.uk>
2023-03-09 15:42:09 +0000

    add enum

---
+++ src/Enums/SkillName.java
@@ -0,0 +1,13 @@
+package Enums;
+
+public enum SkillName {
+    JAVA,
+    PYTHON,
+    SQL,
+    ASSEMBLY,
+    CLOUD_COMPUTING,
+    CLOUD_SECURITY,
+    GIT,
+    NO_SQL
+    
+}
---
+++ src/Enums/WorkingDay.java
@@ -0,0 +1,10 @@
+package Enums;
+
+public enum WorkingDay {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY
+    
+}
