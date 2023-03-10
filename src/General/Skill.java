269ceee151b1e2fa80d796589dfa8a52de1d74c0
Collins Otoo <2746102o@student.gla.ac.uk>
2023-03-10 17:18:23 +0000

    setting getters and setters for skill and degree

--- src/General/Skill.java
+++ src/General/Skill.java
@@ -6,4 +6,12 @@ public class Skill {
     public Skill(String name) {
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
