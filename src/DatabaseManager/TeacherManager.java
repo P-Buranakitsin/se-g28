669cf5e5d18a44f36f2004d8fdc69eb08a136c44
Yana Nedeva <2033575n@student.gla.ac.uk>
2023-03-16 21:54:28 +0000

    commiting edited TeacherManager class

---
+++ src/DatabaseManager/TeacherManager.java
@@ -0,0 +1,86 @@
+package DatabaseManager;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import General.*;
+
+class TeacherManager extends DatabaseManager<Teacher> implements Serializable {
+
+    protected TeacherManager() {}
+
+    @Override
+    public ArrayList<Teacher> readFile() {
+        String filePath = "src/Database/Teachers.tmp";
+        try (
+                ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath))) {
+            try {
+                ArrayList<Teacher> teachers = (ArrayList<Teacher>) ois.readObject();
+                return teachers;
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void writeFile(ArrayList<Teacher> teachers) {
+        String filePath = "src/Database/Teachers.tmp";
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
+            oos.writeObject(teachers);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void view() {
+        ArrayList<Teacher> teachers = readFile();
+        if (teachers != null) {
+            for (int i = 0; i < teachers.size(); i++) {
+                System.out.println(teachers.get(i).toString());
+            }
+        } else {
+            System.out.println("There is no teacher in this database.");
+        }
+    }
+
+    @Override
+    public void remove(int id) {
+        ArrayList<Teacher> teachers = readFile();
+        for (Teacher teacher : teachers) {
+            if (teacher.getId() == id) {
+                teachers.remove(teacher);
+                break;
+            }
+        }
+        writeFile(teachers);
+    }
+
+    @Override
+    public void add(Teacher teacher) {
+        ArrayList<Teacher> teachers = readFile();
+        teachers.add(teacher);
+        writeFile(teachers);
+    }
+
+    @Override
+    public void edit(int id, Teacher teacher) {
+        ArrayList<Teacher> teachers = readFile();
+        for (int i = 0; i < teachers.size(); i++) {
+            if(teachers.get(i).getId() == id) {
+                teachers.set(i, teacher);
+                break;
+            }
+        }
+        writeFile(teachers);
+    }
+}
