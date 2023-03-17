75ecacecdda56fa2bfa4603393e6c97dc6d5b9a3
Yana Nedeva <2033575n@student.gla.ac.uk>
2023-03-17 01:05:34 +0000

    Update src/DatabaseManager/TeacherManager.java

--- src/DatabaseManager/TeacherManager.java
+++ src/DatabaseManager/TeacherManager.java
@@ -9,78 +9,86 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import General.*;
 
+
 class TeacherManager extends DatabaseManager<Teacher> implements Serializable {
 
+    //constructor
     protected TeacherManager() {}
 
     @Override
-    public ArrayList<Teacher> readFile() {
-        String filePath = "src/Database/Teachers.tmp";
+    // method to read an array list of teachers 
+    public ArrayList<Teacher> readFile() { 
+        String filePath = "src/Database/Teachers.tmp"; //assigning file path where the list of teachers is stored
         try (
                 ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath))) {
-            try {
-                ArrayList<Teacher> teachers = (ArrayList<Teacher>) ois.readObject();
-                return teachers;
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
+            try { 
+                ArrayList<Teacher> teachers = (ArrayList<Teacher>) ois.readObject(); //reads the array list of teachers from the file 
+                return teachers; 
+            } catch (ClassNotFoundException e) { //if an exception occurs
+                e.printStackTrace();   //show the stack trace
             }
 
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException e) { //if an exception occurs
+            e.printStackTrace();  // show stack trace
         }
-        return null;
+        return null; // if reading fails, return null
     }
 
     @Override
+    //this method writes an array list of teachers to a file
     protected void writeFile(ArrayList<Teacher> teachers) {
-        String filePath = "src/Database/Teachers.tmp";
+        String filePath = "src/Database/Teachers.tmp"; //assigning file path where the list of teachers is stored
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
-            oos.writeObject(teachers);
-        } catch (IOException e) {
-            e.printStackTrace();
+            oos.writeObject(teachers); //write the teachers to the file
+        } catch (IOException e) { //if an exception occurs
+            e.printStackTrace(); //show the stack trace
         }
     }
 
     @Override
+    //method to see teacher's information
     public void view() {
-        ArrayList<Teacher> teachers = readFile();
-        if (teachers != null) {
-            for (int i = 0; i < teachers.size(); i++) {
-                System.out.println(teachers.get(i).toString());
+        ArrayList<Teacher> teachers = readFile(); //reading the list of teachers from the file 
+        if (teachers != null) { //if the list of teachers is not empty
+            for (int i = 0; i < teachers.size(); i++) { //iterate through the teachers list
+                System.out.println(teachers.get(i).toString()); //and print each teacher's information
             }
-        } else {
+        } else { // if the list of teachers is empty
             System.out.println("There is no teacher in this database.");
         }
     }
 
     @Override
+    //method to remove a teacher from the list of teachers
     public void remove(int id) {
-        ArrayList<Teacher> teachers = readFile();
-        for (Teacher teacher : teachers) {
-            if (teacher.getId() == id) {
-                teachers.remove(teacher);
-                break;
+        ArrayList<Teacher> teachers = readFile(); //reading the list of teachers from the file 
+        for (Teacher teacher : teachers) { //iterating through every teacher in the list
+            if (teacher.getId() == id) { //checking if the id of the current teacher matches the id passed 
+                teachers.remove(teacher); // if it does, remove the teacher from the list
+                break; //terminate the loop
             }
         }
-        writeFile(teachers);
+        writeFile(teachers); // write the updated list of teachers to the file
     }
 
     @Override
+    //method to add a new teacher to the list of teachers
     public void add(Teacher teacher) {
-        ArrayList<Teacher> teachers = readFile();
-        teachers.add(teacher);
-        writeFile(teachers);
+        ArrayList<Teacher> teachers = readFile(); //reading the list of teachers from the file 
+        teachers.add(teacher); //adds a new teacher to the teachers' list
+        writeFile(teachers); //writing the updated teachers' list to the file
     }
 
     @Override
+    //method to update the teacher with a certain id with a new teacher in the list
     public void edit(int id, Teacher teacher) {
-        ArrayList<Teacher> teachers = readFile();
-        for (int i = 0; i < teachers.size(); i++) {
-            if(teachers.get(i).getId() == id) {
-                teachers.set(i, teacher);
-                break;
+        ArrayList<Teacher> teachers = readFile(); //reading the list of teachers from the file 
+        for (int i = 0; i < teachers.size(); i++) { //iterating through every teacher in the list
+            if(teachers.get(i).getId() == id) { //checking if the id matches
+                teachers.set(i, teacher); // if it does, replace teacher at i position in the list with the new teacher
+                break; //terminate the loop
             }
         }
-        writeFile(teachers);
+        writeFile(teachers); // write the updated list of teachers to the file
     }
 }
